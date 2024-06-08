24b08a9c75322f39e1e280f33f845c6e5f1efa74
Adam Daly <you@example.com>
2024-06-08 18:12:15 +0100

    Convert board related code into class, size, initialization and print included

--- run.py
+++ run.py
@@ -16,11 +16,36 @@ for i in range(BOARD_SIZE + 1):
             else: 
                 board[i][j] = str(BOARD_SIZE + 1 - i)
 
+class Board:
+    def __init__(self, size=10):
+        self.size = size
+        self.board = [[" -" for _ in range(BOARD_SIZE + 1)] for _ in range(BOARD_SIZE + 1)]
+        self._initialize_board()
+    
+    def _initialize_board(self):
+        for i in range(self.size + 1):
+            for j in range (self.size + 1):
+                if i == 0 and j == 0:
+                    self.board[i][j] = "  "
+                elif i == 0 and j > 0:
+                    self.board[i][j] = " " + chr(j + 64)
+                elif j == 0:
+                    if (self.size + 1 - i) <= 9:
+                        self.board[i][j] = " " + str(self.size + 1 - i)
+                    else: 
+                        self.board[i][j] = str(self.size + 1 - i)
+    
+    def print(self):
+        for row in self.board:
+            print(" ".join(row)) 
+
 # Entry point for the program
 def main():
     for row in board:
         print(" ".join(row))
 
+    player_board = Board()
+    player_board.print()
 
 # If the script is run directly, call main
 if __name__ == "__main__":
