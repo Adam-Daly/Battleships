586ee31c515a31cd0e870ef8faf0b97ade7b0b1a
Adam Daly <you@example.com>
2024-06-08 16:53:21 +0100

    Fix board initialization with list comprehension, add comments

--- run.py
+++ run.py
@@ -16,7 +16,10 @@ for row in chessboard:
     print(row)
 
 BOARD_SIZE = 10
-board = []
+
+# Fill a list of lists with a symbol to represent blank spaces
+# BOARD_SIZE + 1 is used to accomodate an extra row and col for chessboard notation "A1" etc
+board = [["-" for _ in range(BOARD_SIZE + 1)] for _ in range(BOARD_SIZE + 1)]
 
 for i in range(BOARD_SIZE + 1):
     for j in range (BOARD_SIZE + 1):
