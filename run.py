ddc063209d424ca7be27f5835cabfe878e26687c
Adam Daly <you@example.com>
2024-06-08 17:13:36 +0100

    Add leading spaces for visual clarity and alignment. Replace corner symbol for consistency.

--- run.py
+++ run.py
@@ -19,14 +19,14 @@ BOARD_SIZE = 10
 
 # Fill a list of lists with a symbol to represent blank spaces
 # BOARD_SIZE + 1 is used to accomodate an extra row and col for chessboard notation "A1" etc
-board = [["-" for _ in range(BOARD_SIZE + 1)] for _ in range(BOARD_SIZE + 1)]
+board = [[" -" for _ in range(BOARD_SIZE + 1)] for _ in range(BOARD_SIZE + 1)]
 
 for i in range(BOARD_SIZE + 1):
     for j in range (BOARD_SIZE + 1):
         if i == 0 and j == 0:
-            board[i][j] = "BS"
+            board[i][j] = "  "
         elif i == 0 and j > 0:
-            board[i][j] = chr(j + 64)
+            board[i][j] = " " + chr(j + 64)
         elif j == 0:
             if (BOARD_SIZE + 1 - i) <= 9:
                board[i][j] = " " + str(BOARD_SIZE + 1 - i)
