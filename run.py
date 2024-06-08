9804e5a20460dad8ae8ee0c933404bad31a92da6
Adam Daly <you@example.com>
2024-06-08 18:26:59 +0100

    Remove unnecessary board code

--- run.py
+++ run.py
@@ -1,27 +1,13 @@
-BOARD_SIZE = 10
-
-# Fill a list of lists with a symbol to represent blank spaces
-# BOARD_SIZE + 1 is used to accomodate an extra row and col for chessboard notation "A1" etc
-board = [[" -" for _ in range(BOARD_SIZE + 1)] for _ in range(BOARD_SIZE + 1)]
-
-for i in range(BOARD_SIZE + 1):
-    for j in range (BOARD_SIZE + 1):
-        if i == 0 and j == 0:
-            board[i][j] = "  "
-        elif i == 0 and j > 0:
-            board[i][j] = " " + chr(j + 64)
-        elif j == 0:
-            if (BOARD_SIZE + 1 - i) <= 9:
-               board[i][j] = " " + str(BOARD_SIZE + 1 - i)
-            else: 
-                board[i][j] = str(BOARD_SIZE + 1 - i)
-
+# Game board class to be used by the player and computer
 class Board:
     def __init__(self, size=10):
         self.size = size
+        # Fill a list of lists with a symbol to represent blank spaces
+        # self.size + 1 is used to accomodate an extra row and col for chessboard notation "A1" etc
         self.board = [[" -" for _ in range(self.size + 1)] for _ in range(self.size + 1)]
         self._initialize_board()
     
+    # Set row 0 and col 0 to chess notation, 
     def _initialize_board(self):
         for i in range(self.size + 1):
             for j in range (self.size + 1):
@@ -41,9 +27,6 @@ class Board:
 
 # Entry point for the program
 def main():
-    for row in board:
-        print(" ".join(row))
-
     player_board = Board()
     player_board.print()
 
