bf567069ca182e17d8275a2da8ac5b8e929ba02f
Adam Daly <you@example.com>
2024-06-09 12:42:02 +0100

    Add more comments to place_ship method

--- run.py
+++ run.py
@@ -21,29 +21,38 @@ class Board:
                     else: 
                         self.board[i][j] = str(self.size + 1 - i)
 
-    # Takes an origin location, a ship size and an orientation
+    # Takes an origin location, a ship size and an orientation ("H" for horizontal, "V" for vertical)
     # Check if locations are empty and place ship, return False otherwise
+    # Ships will only ever be placed left to right, or top to bottom
     def place_ship(self, board_row : int, board_col : int, ship_size : int, orientation : str) -> bool:
         positions = []
         if orientation == "H":
-            # Check if ship is size exceeds board size
+            # Check if ship can fit at chosen position
             if board_col + ship_size > self.size:
                 return False
+            # Loop through positive direction in horizontal orientation
+            # Return False if not a valid position
+            # Add position to list if valid
             for i in range(ship_size):
                 if self.board[board_row][board_col + i] != " -":
                     return False
                 else:
                     positions.append((board_row, board_col + i))
         elif orientation == "V":
+            # Check if ship can fit at chosen position
             if board_row + ship_size > self.size:
                 return False
+            # Loop through positive direction in vertical orientation 
+            # Return False if not a valid position
+            # Add position to list if valid
             for i in range(ship_size):
                 if self.board[board_row + i][board_col] != " -":
                     return False
                 else:
                     positions.append((board_row + i, board_col))
+        # If we haven't returned yet, all positions are valid so we place the ship
         for (row_pos, col_pos) in positions:
-            self.board[row_pos][col_pos] = " X"
+            self.board[row_pos][col_pos] = " X" 
 
     def print(self):
         for row in self.board:
