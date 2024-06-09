7a2c74e5ade81570807f403838b1fa8ebafbf5b4
Adam Daly <you@example.com>
2024-06-09 12:05:40 +0100

    Add method to place ships, add test for placing ships in main

--- run.py
+++ run.py
@@ -20,7 +20,30 @@ class Board:
                         self.board[i][j] = " " + str(self.size + 1 - i)
                     else: 
                         self.board[i][j] = str(self.size + 1 - i)
-    
+
+    # Check if locations are empty and place ship, return False otherwise
+    def place_ship(self, x, y, size, direction):
+        positions = []
+        # direction is either "H" for horizontal or "V" for vertical
+        if direction == "H":
+            if y + size > self.size:
+                return False
+            for i in range(size):
+                if self.board[x][y + i] != " -":
+                    return False
+                else:
+                    positions.append((x, y + i))
+        elif direction == "V":
+            if x + size > self.size:
+                return False
+            for i in range(size):
+                if self.board[x + i][y] != " -":
+                    return False
+                else:
+                    positions.append((x + i, y))
+        for (x_pos, y_pos) in positions:
+            self.board[x_pos][y_pos] = " X"
+
     def print(self):
         for row in self.board:
             print(" ".join(row)) 
@@ -29,6 +52,11 @@ class Board:
 def main():
     player_board = Board()
     player_board.print()
+
+    if player_board.place_ship(2, 2, 5, "H") == False:
+        print("Cannot place ship!")
+    else:
+        player_board.print()
 
 # If the script is run directly, call main
 if __name__ == "__main__":
